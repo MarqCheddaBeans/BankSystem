e1f6a361f6987e67ed9416281471d80dfc627ecd
IsaiahMV27 <mvillanueva@my.yearupunited.org>
2025-10-22 19:41:05 -0400

    First commit, added BankAccount and Main class

---
+++ src/main/java/com/pluralsight/BankAccount.java
@@ -0,0 +1,70 @@
+package com.pluralsight;
+
+//Create a BankAccount class with private fields: accountNumber, accountHolder, balance, isActive
+//Implement public getter methods for safe data access
+//Create public methods: deposit(), withdraw(), getAccountInfo()
+//Include private helper methods: validateAmount(), updateBalance()
+
+import java.util.Scanner;
+
+public class BankAccount {
+    Scanner scan = new Scanner(System.in);
+
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+    private boolean isActive;
+
+    public BankAccount(int accountNumber, String accountHolder, double balance, boolean isActive) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.isActive = isActive;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void deposit(){
+
+        System.out.print("Please enter deposit amount: ");
+        double amount = scan.nextDouble();
+
+        this.balance += amount;
+
+        System.out.println("Deposit successful!");
+        System.out.println("New Balance: " + this.balance);
+
+    }
+
+    public void withdraw(){
+
+        System.out.println("Please enter withdrawal amount");
+         double amount = scan.nextDouble();
+
+         if(balance < amount){
+             System.out.println("You dont got it like that, try again.");
+         }else{
+             amount *= -1;
+             balance += amount;
+             System.out.println("Withdrawal successful!");
+             System.out.println("Your balance is: " + balance);
+         }
+
+
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount oneAccount = new BankAccount(1234,"Marques",500,true);
+
+    }
+
+}
